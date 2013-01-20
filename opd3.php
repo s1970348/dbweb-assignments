f47d5acdffc830a4c463b6f6268d30ec8eda1e8e
Jesse Veentjer <j.j.veentjer@student.rug.nl>
2013-01-20 14:18:15 +0100

    final version assignment 3

--- opd3.php
+++ opd3.php
@@ -13,41 +13,42 @@ require_once('dbconnect.php');
 //or $_SESSION['active']>60*60*24*7
 if (!isset($_SESSION['active'])) { //1st time or expired or when reloading 1st question
 //Workaround for first iteration
-$_REQUEST['public'] = 'pie';
+$_SESSION['public'] = '42';
 $_SESSION['qnum']=1;
 $_SESSION['ncorrect']=0;
 $_SESSION['active']=$_SERVER["REQUEST_TIME"];
-$querynumber=1;
+$_SESSION['querynumber']=1;
 } else {//not 1st or expired
+
 $_SESSION['active']=$_SERVER["REQUEST_TIME"];
 if (isset($_POST['postedqnum'])){
 $_SESSION['qnum']=$_POST['postedqnum'];
-}
+}} 
 if ($_SESSION['qnum'] != 1){
-$querynumber = $_SESSION['qnum'] -1;
+$_SESSION['querynumber'] = $_SESSION['qnum'] -1;
+$_SESSION['public']= $_REQUEST['public'];
+// having trouble finding a way around the $_REQUEST['public'] .. this causes an error on reloading 
 }
-$query1=("SELECT * FROM `choice` WHERE correct = 1 and question_number = ".$querynumber);
+$query1=("SELECT * FROM `choice` WHERE correct = 1 and question_number = ".$_SESSION['querynumber']);
 $answerresult = mysql_query($query1) or die ('GOED ANTWOORD QUERY FAALT'. mysql_error());
 $answer_row= mysql_fetch_array($answerresult) or die ('Goed antwoord vertalen faalt'. mysql_error());
-$correct_answer= $answer_row['c_text'];
 
-
-if ($correct_answer == $_REQUEST['public']){
+if ($answer_row['c_text'] == $_SESSION['public']){
+    //check if answer is correct
     ++$_SESSION['ncorrect'];
     }
-}
+
 
 // Is there a way to query for number of rows?
 if ($_SESSION['qnum'] == 4){
-echo('klaar met de QUIZ <BR /> You answered : '.$_SESSION['ncorrect'].'answers out of '.$querynumber.' correctly!');
+echo('klaar met de QUIZ <BR /> You answered : '.$_SESSION['ncorrect'].'answers out of '.$_SESSION['querynumber'].' correctly!');
 session_destroy();
 }else {
     
-$query=('SELECT * FROM  `question` where q_number ='.$_SESSION['qnum']);
+$query=('SELECT * FROM  `s1970348`.`question` where q_number ='.$_SESSION['qnum']);
 $result= mysql_query($query) or die('QUESTION OPVRAGEN FAALT' . mysql_error());
 $row = mysql_fetch_array($result) or die ('ROW VERTALEN FAALT' . mysql_error());
 $question = $row['q_text'];
-$question_number = $row['q_number'];
 
 $questionarray= array() ;
 $query2=("SELECT c_text FROM `choice` where `question_number` = ".$_SESSION['qnum'] );
@@ -57,6 +58,7 @@ while($question_row = mysql_fetch_array($questionqueryresult)){
 array_push($questionarray , $question_row['c_text']);
 }
 $postqnum =$_SESSION['qnum'] + 1;
+// can this arithmic be done in an echo directly?
 echo("
 <fieldset id='question'>
 <legend>Question number : ". $_SESSION['qnum'] ." </legend>
@@ -76,6 +78,7 @@ echo("
 
 </form>
 </fieldset> ");
+mysql_close($link); 
 }
 ?>
 
